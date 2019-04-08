e6c9ea6531cbc367bf78f408d1563c78f13caafa
Benjamin Easley <bceasley@unix1.csc.calpoly.edu>
2019-04-08 13:22:47 -0700

    Location tests done

--- location_tests.py
+++ location_tests.py
@@ -6,8 +6,21 @@ class TestLab1(unittest.TestCase):
     def test_repr(self):
         loc = Location("SLO", 35.3, -120.7)
         self.assertEqual(repr(loc),"Location('SLO', 35.3, -120.7)")
-    
+        loc = Location("Paris", 1, 2)
+        self.assertEqual(repr(loc), "Location('Paris', 1, 2)")
     # Add more tests!
+    #def test_init(self, name, lat, lon):
+        #self.assertAlmostEqual(init("Paris",   
+
+    def test_eq(self):
+        loc1 = Location("SLO", 3, 4)
+        loc2 = Location("Italy", -5, 4.5)
+        loc3 = loc1
+        loc4 = Location("Italy", -5, 4.5)
+        self.assertFalse(loc1 == loc2)
+        self.assertTrue(loc2 == loc4)
+        self.assertTrue(loc1 == loc3)
+
 
 if __name__ == "__main__":
         unittest.main()
